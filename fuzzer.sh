a3610e40ba80beac64e1ada5c05721c59f3d50ec
ske1337 <ske1337@gmail.com>
2024-12-13 08:10:32 +0100

    Guard sh argument count

--- fuzzer.sh
+++ fuzzer.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ "$#" -ne 3 ]; then
+    echo "Usage: fuzzer.sh <input-file> <output-file> <tested-app>"
+    exit 1
+fi
+
 while true; do
     ./mut.py "$1" "$2" > "$2.log"
     "$3" "$2"

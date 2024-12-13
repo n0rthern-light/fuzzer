8231da4c6cc0eded852a90e265c9e5baf2a5fff8
ske1337 <ske1337@gmail.com>
2024-12-13 08:36:24 +0100

    Fix path issues when called from symlink and different work dir

--- fuzzer.sh
+++ fuzzer.sh
@@ -5,13 +5,31 @@ if [ "$#" -ne 3 ]; then
     exit 1
 fi
 
+input_file=$(realpath "$1")
+output_file="$(dirname "$input_file")/$2"
+mutation_log_file="$output_file.log"
+tested_app=$(realpath "$3")
+
+resolve_symlink() {
+    target_file="$1"
+    while [ -L "$target_file" ]; do
+        target_file=$(readlink "$target_file")
+        case "$target_file" in
+            /*) : ;;
+            *) target_file=$(dirname "$1")/"$target_file" ;;
+        esac
+    done
+    echo "$(cd "$(dirname "$target_file")" && pwd)"
+}
+script_dir=$(resolve_symlink "$0")
+
 while true; do
-    ./mut.py "$1" "$2" > "$2.log"
-    "$3" "$2"
+    "$script_dir/mut.py" "$input_file" "$output_file" > "$mutation_log_file"
+    "$tested_app" "$output_file"
     if [ "$?" -ne "0" ]; then
         echo CRASH CRASH CRASH
-        echo Mutation that did the crash:
-        cat "$2.log" 
+        echo Mutation that did the crash available in: "$mutation_log_file" 
+        exit 0
     fi
 done
 
